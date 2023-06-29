1a7ea00704745ccd0a38330ac41d6b3917297d39
samar3366 <sumanthreddypathi3366@gmail.com>
2023-06-29 14:35:43 +0530

    client - 1 addition

---
+++ client-1/scripts/test.js
@@ -0,0 +1,9 @@
+const data = {
+    name: "Joe",
+    age: 52,
+    place: "France"
+}
+
+const showDetails = () => {
+    console.log(`name: ${data.name}, age: ${data.age}, place: ${data.place}`)
+}
